c58f6544533b20f7978270e59035f74e53870fdf
johnwgomez <johnwgomez10@gmail.com>
2025-06-01 21:17:21 -0700

    finalized everything

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
 // TODO: Include packages needed for this application
+// inquirer used for command prompt lines
+//fs for file system interaction
+// 
+import inquirer from 'inquirer';
+import fs from 'fs';
+import generateMarkdown from './utils/generateMarkdown.js';
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    { type: 'input', name: 'title', message: 'Project Title:' },
+    { type: 'input', name: 'description', message: 'Project Description:' },
+    { type: 'input', name: 'installation', message: 'Installation Instructions:' },
+    { type: 'input', name: 'usage', message: 'Usage Information:' },
+    { type: 'list', name: 'license', message: 'Choose a license:', choices: ['MIT', 'GPLv3', 'Apache 2.0', 'None'] },
+    { type: 'input', name: 'contributing', message: 'Contribution Guidelines:' },
+    { type: 'input', name: 'tests', message: 'Test Instructions:' },
+    { type: 'input', name: 'github', message: 'GitHub Username:' },
+    { type: 'input', name: 'email', message: 'Email Address:' }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    try {
+        fs.writeFileSync(fileName, data);
+        console.log(`Successfully created ${fileName}!`);
+    } catch (error) {
+        console.error(`Error writing file: ${error.message}`);
+    }
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        const readmeContent = generateMarkdown(answers);
+        writeToFile('README.md', readmeContent);
+    });
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,71 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === 'None') return '';
+  const badges = {
+      MIT: '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)',
+      GPLv3: '![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)',
+      'Apache 2.0': '![License: Apache 2.0](https://img.shields.io/badge/License-Apache_2.0-red.svg)'
+  };
+  return badges[license] || '';
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'None') return '';
+  const links = {
+      MIT: '[MIT License](https://opensource.org/licenses/MIT)',
+      GPLv3: '[GPL v3 License](https://www.gnu.org/licenses/gpl-3.0)',
+      'Apache 2.0': '[Apache 2.0 License](https://www.apache.org/licenses/LICENSE-2.0)'
+  };
+  return links[license] || '';
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === 'None') return '';
+  return `## License
+  
+This project is licensed under the ${license} license.  
+For more details, see: ${renderLicenseLink(license)}`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+${renderLicenseBadge(data.license)}
+
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+${renderLicenseSection(data.license)}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+GitHub: [${data.github}](https://github.com/${data.github})  
+Email: ${data.email} (Reach out for further inquiries!)
 `;
 }
 
